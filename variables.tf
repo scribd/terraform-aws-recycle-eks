578c5eb20aada028105220c8cba7b0c5dca023bf
kuntalkumarbasu <kuntalb@scribd.com>
2020-10-21 02:28:30 -0400

    Removed redundant files and variables

--- variables.tf
+++ variables.tf
@@ -1,39 +1,6 @@
 variable "aws_region" {
   type        = string
-  default     = "us-east-2"
   description = "AWS Region to default resources into"
-}
-
-variable "network_master_vpc" {
-  type        = string
-  description = "VPC from network master to deploy the stack into"
-  default     = "migration"
-}
-
-variable "prefix" {
-  type        = string
-  description = "Prefix for key AWS resources to use, such as the developers user name"
-}
-
-variable "owner" {
-  type        = string
-  description = "Tag to use for helping map owners to their resources"
-}
-
-variable "department" {
-  type        = string
-  description = "Tag to use for helping map department to their resources"
-}
-
-variable "env" {
-  type        = string
-  description = "Environment prefix"
-}
-
-variable "instance_type" {
-  type        = string
-  default     = "m4.large"
-  description = "Instance type for the worker nodes"
 }
 
 # state machine variables
